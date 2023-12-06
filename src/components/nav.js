e4c81ccc59be18ca723191a7fedef4c0391c2b81
rxjans <120579803+rxjans@users.noreply.github.com>
2023-12-06 19:08:48 +0530

    add change

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import logo from '../assets/logo.jpg';
+import user from '../assets/logo.jpg';
+import logo from '../assets/logo.png';
 
 const nav = () => {
   return (
-    <nav className='flex w-full justify-between px-12 border-b-2'>
-        <div><h1>expensify.io</h1></div>
-        <div className='flex'>
+    <nav className='flex w-full justify-between px-12 border-b-2 bg-blue-800 py-2'>
+        <div className=''><img className='w-[200px] h-[48px]' src={logo}></img></div>
+        <div className='flex text-white font-mono'>
             <div className='mr-4'>
-            <h1>username</h1>
-            <h6>credit score</h6>
+            <h1>USERNAME</h1>
+            <h6>CREDIT SCORE</h6>
             </div>
             <div className='border border-b shadow-lg rounded-full w-[52px] '>
-                <img className='m-auto rounded-full w-[36px] h-[48px]' src={logo}></img>
+                <img className='m-auto rounded-full h-[48px]' src={user}></img>
             </div>
         </div>
     </nav>
